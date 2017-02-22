dc5031685cef7334251ea2004c2b9d90567f3b15
Andreas <ogge_7@hotmail.com>
2017-02-22 10:46:38 +0100

    basic

---
+++ MVC1new/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC1new.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
